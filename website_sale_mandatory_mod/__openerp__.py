60824f0bf0097489c894bd5245a7957158b27f58
KontorConsulting <p.becker@kontor.consulting>
2015-10-16 12:06:20 +0200

    Create __openerp__.py

---
+++ website_sale_mandatory_mod/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'eCommerce Mandatory Mod',
+    'category': 'Website',
+    'summary': 'Modify Mandatory and Optional Billing and Shipping Fields',
+    'website': 'http://kontor.consulting',
+    'version': '1.0',
+    'description': """
+ Website Sale Mod
+==================
+
+        """,
+    'author': 'Kontor Consulting GmbH',
+    'depends': ['website_sale'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+}
