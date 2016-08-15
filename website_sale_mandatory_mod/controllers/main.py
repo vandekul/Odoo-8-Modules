f1f9172a98866540f5f613a66bfbde9db655b36f
Vande <vandekul@users.noreply.github.com>
2016-08-15 21:57:13 +0200

    Update main.py

--- website_sale_mandatory_mod/controllers/main.py
+++ website_sale_mandatory_mod/controllers/main.py
@@ -12,7 +12,7 @@ class website_sale_mandatory_mod(main.website_sale):
     @http.route(['/shop/confirm_order'], type='http', auth="public", website=True)
     def confirm_order(self, **post):
         self.mandatory_billing_fields  = ["name", "email", "street2", "city", "country_id", "zip"]
-        self.optional_billing_fields   = ["street", "state_id", "vat", "vat_subjected"]
+        self.optional_billing_fields   = ["street", "state_id", "vat"]
         self.mandatory_shipping_fields = ["name", "street", "city", "country_id", "zip"]
         self.optional_shipping_fields  = ["state_id"]
         return super(website_sale_mandatory_mod,self).confirm_order(**post)
